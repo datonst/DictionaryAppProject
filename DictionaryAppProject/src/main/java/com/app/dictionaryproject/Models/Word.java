e3b700c43930c3cac7025d86d46c2e53202e2854
nvnhat04 <144301474+nvnhat04@users.noreply.github.com>
2023-09-28 17:47:52 +0700

    Update Word.java

--- DictionaryAppProject/src/main/java/com/app/dictionaryproject/Models/Word.java
+++ DictionaryAppProject/src/main/java/com/app/dictionaryproject/Models/Word.java
@@ -1,4 +1,9 @@
 package com.app.dictionaryproject.Models;
 
 public class Word {
+    private String word_target;
+    private  String pronunciation;
+    String getWordTarget() {
+        return word_target;
+    }
 }
