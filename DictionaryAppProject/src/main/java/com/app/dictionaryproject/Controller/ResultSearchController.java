a7625962fa517970e48386a8e40e13f3943d53a6
nvnhat04 <144301474+nvnhat04@users.noreply.github.com>
2023-11-15 11:42:09 +0700

    fix UI Explain Screen

--- DictionaryAppProject/src/main/java/com/app/dictionaryproject/Controller/ResultSearchController.java
+++ DictionaryAppProject/src/main/java/com/app/dictionaryproject/Controller/ResultSearchController.java
@@ -4,11 +4,16 @@ import com.app.dictionaryproject.Models.Word;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.LightBase;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -31,19 +36,39 @@ public class ResultSearchController {
     public Label words = new Label();
     @FXML
     public Label phonetic = new Label();
-    public ScrollPane explain = new ScrollPane();
+    public Label type = new Label();
+    public Label explain = new Label();
+    public Label synonym = new Label();
+    public Label antonym = new Label();
+    public VBox vBox = new VBox();
+    public  ScrollPane scrollPane = new ScrollPane();
     @FXML
-    public void initialize (Word word) {
+    public void initialize(Word word) {
         if (word != null) {
             words.setText(word.getWord_target());
             phonetic.setText("Phonetic: " + word.getPhonetic());
-            String ex = "Loại từ: " + word.getWordType().replace("\n", ",") + "\n\n";
-            String other = "Từ đồng nghĩa: " + word.getSynonym().replace(" ,", ", ") + "\n\n"
-                      + "Từ trái nghĩa: " + word.getAntonym().replace(" ,", ", ") + "\n\n";
-            Label temp = new Label(ex + word.getDefinitionWord() + "\n\n" + other);
-            explain.setContent(temp);
+            String ex = "Loại từ: " + word.getWordType().replace("\n", " ");
+            String sym = "Từ đồng nghĩa: " + word.getSynonym().replace(" ,", ", ");
+            String anto = "Từ trái nghĩa: " + word.getAntonym().replace(" ,", ", ");
+            type.setText(ex);
+            type.getStyleClass().add("word");
+
+            synonym.setText(sym);
+            antonym.setText(anto);
+
+            // Use Label with wrapText set to true for multiline text
+            explain.setText("Define: " + word.getDefinitionWord());
+            explain.setWrapText(true);
+
+            // Clear previous content and add new labels
+            vBox.getChildren().clear();
+            vBox.getChildren().addAll(type,  explain, synonym, antonym);
+            vBox.setSpacing(10);
+            scrollPane.setContent(vBox);
+
         }
     }
+
     public void textToSpeech(String textToSpeak) {
         // thay đổi theo từng máy của mọi người
         String command = "cscript.exe /nologo  " + System.getProperty("user.dir") + "\\src\\main\\resources\\data\\TTSAPI.vbs \"" + textToSpeak + "\"";
